8c5c94fb392f29f3338bbcc5e2ae83d5b4fe792b
tarek doha <tarikdoha80@gmail.com>
2022-01-14 14:15:47 +0100

    Enqueue Update Console for HangFire Expressions

--- MobilePayAPI/Controllers/MerchantsController.cs
+++ MobilePayAPI/Controllers/MerchantsController.cs
@@ -16,18 +16,16 @@ namespace MobilePayAPI.Contrllers
         private readonly IFeeService _feeService;
         private readonly IMapper _mapper;
         private readonly IBackgroundJobClient _backgroundJobClient;
-        private readonly IRecurringJobManager _recurringJobManager;
         public decimal total = 0;
         public MerchantsController(IMerchantService merchantService,
             IMapper mapper,
             IFeeService feeService,
-            IBackgroundJobClient backgroundJobClient,
-            IRecurringJobManager recurringJobManager)
+            IBackgroundJobClient backgroundJobClient)
         {
             _merchantService = merchantService;
             _mapper = mapper;
             _backgroundJobClient = backgroundJobClient;
-            _recurringJobManager = recurringJobManager;
+
         }
         [HttpPost]
         public ActionResult<MerchantReadDto> CreateMerchant(MerchantCreateDto merchantCreateDto)
@@ -44,6 +42,7 @@ namespace MobilePayAPI.Contrllers
             var merchanatItems = _merchantService.GetMerchants();
             foreach (var merchant in merchanatItems)
             {
+                _backgroundJobClient.Enqueue(() => GetMerchants());
                 _backgroundJobClient.Enqueue(() => 
                 Console.WriteLine($"Merchant Items Enqueue : " +
                 $"Merchant ID:{0}, Metrchant Name:{1}, Merchant Amount: {2}, Merchant TimeSpan: {3}"
@@ -56,7 +55,7 @@ namespace MobilePayAPI.Contrllers
         public ActionResult<MerchantReadDto> GetMerchant(Guid id)
         {
             var merchantItem = _merchantService.GetMerchant(id);
-            _backgroundJobClient.Enqueue(() => Console.WriteLine($"Merchant Item {1}", merchantItem));
+            _backgroundJobClient.Enqueue(() => _merchantService.GetMerchant(id));
             return Ok(_mapper.Map<MerchantReadDto>(merchantItem));
         }
 
@@ -98,7 +97,7 @@ namespace MobilePayAPI.Contrllers
                     decimal discount = Convert.ToDecimal(0.25) * charge;
                     decimal Volume = Convert.ToDecimal(0.20) * (charge - discount);
                     decimal Totalfee = charge - discount - Volume;
-                    _backgroundJobClient.Enqueue(() => Console.WriteLine($"Tesla Exceed 10 Transactions monthly,The description bill is:" + "1% fee charge of {total} DKK = {charge}, 25% Tesla discount of {total} DKK = {discount},Transaction is during a Friday, so the fee will not be free,20% volume discount of {charge} - {discount} = {Volume}, Total fee = {total}DKK - {charge}DKK - {Volume}DKK - {discount}DKK = {Totalfee} DKK ", total, charge, discount, Volume, Totalfee));
+                    RecurringJob.AddOrUpdate(() => Console.WriteLine($"Tesla Exceed 10 Transactions monthly,The description bill is:" + $"1% fee charge of {total} DKK = {charge}, 25% Tesla discount of {total} DKK = {discount},Transaction is during a Friday, so the fee will not be free,20% volume discount of {charge} - {discount} = {Volume}, Total fee = {total}DKK - {charge}DKK - {Volume}DKK - {discount}DKK = {Totalfee} DKK ", total, charge, discount, Volume, Totalfee), Cron.Monthly);
                 }
                 else
                 {
@@ -106,6 +105,7 @@ namespace MobilePayAPI.Contrllers
                     decimal discount = Convert.ToDecimal(0.20) * charge;
                     decimal Volume = (charge - discount);
                     decimal Totalfee = charge - discount - Volume;
+                    RecurringJob.AddOrUpdate(() => Console.WriteLine($"Tesla not Exceed 10 Transactions monthly,The description bill is:" + $"1% fee charge of {total} DKK = {charge}, 25% Tesla discount of {total} DKK = {discount},Transaction is during a Friday, so the fee will not be free,{charge} - {discount} = {Volume}, Total fee = {total}DKK - {charge}DKK - {Volume}DKK - {discount}DKK = {Totalfee} DKK ", total, charge, discount, Volume, Totalfee), Cron.Monthly);
                 }
                 if (merchantName == "Rema1000" && merchanatItems.Count() > 9 && DateTime.Now! > expiryDate)
                 {
@@ -113,6 +113,7 @@ namespace MobilePayAPI.Contrllers
                     decimal discount = Convert.ToDecimal(0.15) * charge;
                     decimal Volume = Convert.ToDecimal(0.20) * (charge - discount);
                     decimal Totalfee = charge - discount - Volume;
+                    RecurringJob.AddOrUpdate(() => Console.WriteLine($"Rema1000 Exceed 10 Transactions monthly,The description bill is:" + $"1% fee charge of {total} DKK = {charge}, 25% Tesla discount of {total} DKK = {discount},Transaction is during a Friday, so the fee will not be free,20% volume discount of {charge} - {discount} = {Volume}, Total fee = {total}DKK - {charge}DKK - {Volume}DKK - {discount}DKK = {Totalfee} DKK ", total, charge, discount, Volume, Totalfee), Cron.Monthly);
                 }
                 else
                 {
@@ -120,6 +121,7 @@ namespace MobilePayAPI.Contrllers
                     decimal discount = Convert.ToDecimal(0.15) * charge;
                     decimal Volume = (charge - discount);
                     decimal Totalfee = charge - discount - Volume;
+                    RecurringJob.AddOrUpdate(() => Console.WriteLine($"Rema1000 not Exceed 10 Transactions monthly,The description bill is:" + $"1% fee charge of {total} DKK = {charge}, 25% Tesla discount of {total} DKK = {discount},Transaction is during a Friday, so the fee will not be free,{charge} - {discount} = {Volume}, Total fee = {total}DKK - {charge}DKK - {Volume}DKK - {discount}DKK = {Totalfee} DKK ", total, charge, discount, Volume, Totalfee), Cron.Monthly);
                 }
                 if (merchantName == "McDonald" && merchanatItems.Count() > 9 && DateTime.Now! > expiryDate)
                 {
@@ -127,6 +129,7 @@ namespace MobilePayAPI.Contrllers
                     decimal discount = Convert.ToDecimal(0.05) * charge;
                     decimal Volume = Convert.ToDecimal(0.20) * (charge - discount);
                     decimal Totalfee = charge - discount - Volume;
+                    RecurringJob.AddOrUpdate(() => Console.WriteLine($"McDonald Exceed 10 Transactions monthly,The description bill is:" + $"1% fee charge of {total} DKK = {charge}, 25% Tesla discount of {total} DKK = {discount},Transaction is during a Friday, so the fee will not be free,20% volume discount of {charge} - {discount} = {Volume}, Total fee = {total}DKK - {charge}DKK - {Volume}DKK - {discount}DKK = {Totalfee} DKK ", total, charge, discount, Volume, Totalfee), Cron.Monthly);
                 }
                 else
                 {
@@ -134,6 +137,7 @@ namespace MobilePayAPI.Contrllers
                     decimal discount = Convert.ToDecimal(0.05) * charge;
                     decimal Volume = (charge - discount);
                     decimal Totalfee = charge - discount - Volume;
+                    RecurringJob.AddOrUpdate(() => Console.WriteLine($"McDonald not Exceed 10 Transactions monthly,The description bill is:" + $"1% fee charge of {total} DKK = {charge}, 25% Tesla discount of {total} DKK = {discount},Transaction is during a Friday, so the fee will not be free,{charge} - {discount} = {Volume}, Total fee = {total}DKK - {charge}DKK - {Volume}DKK - {discount}DKK = {Totalfee} DKK ", total, charge, discount, Volume, Totalfee), Cron.Monthly);
                 }
             }
 
